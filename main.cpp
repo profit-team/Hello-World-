ad5d53c2914347322fa564eaaccff573f50505eb
profit-team <a-vo1@yandex.ru>
2024-05-15 08:12:37 +0300

    Add files via upload
    First addition

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include  <stdio.h>
+#include <string.h>
+ 
+char sz[19]="SONY_Pictures";
+
+//////////////////////////////////////////////////////////
+int main()                                              //
+{
+
+    int n = strlen(sz); 
+     
+    for(int i=0; i < n; i++)
+    {
+   
+        printf("%s\n", &sz[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
